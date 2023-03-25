67b10a4972b2abd101be70a84622b4a0e4616af9
Leighton Tidwell <leighton@tidwells.world>
2023-03-25 13:20:16 -0500

    introduce jest

---
+++ src/Button/Button.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Button } from ".";
+
+describe("Button", () => {
+  it("renders", () => {
+    render(<Button />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+});
