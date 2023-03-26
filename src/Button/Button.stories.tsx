5a20bea0685d3216e3107349acd75b595d10e7d4
Peyton Cleveland <peyton.cleveland@omnifederal.com>
2023-03-25 22:34:32 -0500

    prettier, eslint, button props

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { Button } from '.';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './Button';
 
 export default {
   title: 'Button',
   component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
+
+export const Filled = Template.bind({});
+Filled.args = {
+  children: 'Filled Button',
 };
 
-export const Primary = () => <Button>Primary</Button>;
+export const Outlined = Template.bind({});
+Outlined.args = {
+  kind: 'outlined',
+  children: 'Outlined Button',
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  kind: 'text',
+  children: 'Text Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  children: 'Disabled Button',
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+  children: 'Loading Button',
+};
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
 
-type Props = {
-  children: React.ReactNode;
-};
+export interface Props {
+  children: ReactNode;
+  kind?: 'filled' | 'outlined' | 'text';
+  variant?: 'primary' | 'secondary' | 'danger' | 'success' | 'warning';
+  size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+  loading?: boolean;
+  onClick?: () => void;
+  className?: string;
+  type?: 'button' | 'submit' | 'reset';
+  fullWidth?: boolean;
+  icon?: ReactNode;
+  iconPosition?: 'left' | 'right';
+  [key: string]: unknown;
+}
 
-const Button: React.FC<Props> = ({ children }) => <button>{children}</button>;
+const Button: FC<Props> = ({
+  children,
+  kind = 'filled',
+  variant = 'primary',
+  size = 'medium',
+  disabled = false,
+  loading = false,
+  onClick,
+  className,
+  type = 'button',
+  fullWidth = false,
+  icon,
+  iconPosition = 'left',
+  ...rest
+}) => (
+  <button
+    disabled={disabled}
+    onClick={onClick}
+    type={type}
+    className={className}
+    {...rest}
+  >
+    {children}
+  </button>
+);
 
 export default Button;
